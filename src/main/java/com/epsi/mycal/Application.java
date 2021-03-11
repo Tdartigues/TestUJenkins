41a874339d4949dc8cf2de1322ebd366d7b87111
Tdartigues <73830061+Tdartigues@users.noreply.github.com>
2021-03-11 15:52:51 +0100

    Init Project Merci JL <3

---
+++ src/main/java/com/epsi/mycal/Application.java
@@ -0,0 +1,5 @@
+package com.epsi.mycal;
+
+public class Application {
+    //System.out.println("Suis je en vie ?")
+}
---
+++ src/test/java/com/epsi/mycal/CalculatorTest.java
@@ -0,0 +1,38 @@
+package com.epsi.mycal;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Before
+    public void setUp() throws Exception{
+
+    }
+    @After
+    public void tearDown() {
+
+    }
+    @Test
+    public void testAdd() throws Exception{
+        int a = 2;
+        int b = 3;
+        Calculator calc = new Calculator();
+        int actual = calc.add(a,b);
+
+        assertEquals(5, actual);
+    }
+    @Test
+    public void testSub() throws Exception{
+        int a = 4;
+        int b = 2;
+        Calculator calc = new Calculator();
+        int actual = calc.sub(a,b);
+
+        assertEquals(2, actual);
+    }
+
+}
